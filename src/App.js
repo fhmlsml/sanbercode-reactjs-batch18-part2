374796fe2e668be328f388158b88c033f4091def
fhmlsml <email.gemail10@gmail.com>
2020-10-13 07:57:23 +0700

    tugas11

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tugas9 from './Tugas-9/tugas9';
 import Tugas10 from './Tugas-10/tugas10';
+import Timer from './Tugas-11/tugas11';
 
-class App extends React.Component {
-  render(){
-    return (
+const App = () => {
+  return (
       <div>
         <Tugas9/>
         <Tugas10/>
+        <Timer/>
       </div>
     )
-  }
 }
 
 export default App;
